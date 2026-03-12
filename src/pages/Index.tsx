43ad5effb8f09bc1d2f3f9b304f90daa142bd066
benealexandru2611-netizen <benealexandru2611@gmail.com>
2026-03-12 17:51:18 +0200

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -14,7 +14,6 @@ const Index = () => {
         <SiteHeader />
         <HeroSection />
         <AboutSection />
-        <CategoriesSection />
         <ContactSection />
         <SignatureSection />
         <SiteFooter />
